8f8ddb7a8287f70ea33f116c8984d12c58630f63
gabriel.gruppelli <gabriel.gruppelli@g.globo>
2021-05-14 14:40:03 -0300

    update as root the computing_units

--- admin/app/admin/computing_units.rb
+++ admin/app/admin/computing_units.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register ComputingUnit do
   permit_params :name, :phase, :code, :sampling
   
   form do |f|
-    f.inputs "Player Type" do
+    f.inputs "Computing Unit" do
       f.input :name
       f.input :phase, as: :select, collection: ComputingUnit::PHASE_OPTIONS
       f.input :code, :as => :text
--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: "admin/dashboard#index"
+  root to: "admin/computing_units#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
